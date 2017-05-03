41a6f28667c850b60b0ad6c5093be57665c2ccbb
Andrei Caprar <andrei.caprar@evozon.com>
2017-05-03 17:45:56 +0300

    added requred attributes for active banks

--- MvcApplication/Bundles/Core/Entity/ActiveBank.cs
+++ MvcApplication/Bundles/Core/Entity/ActiveBank.cs
@@ -1,10 +1,19 @@
-﻿namespace MvcApplication.Bundles.Core.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication.Bundles.Core.Entity
 {
     public class ActiveBank
     {
+        [Key]
         public int Id { get; set; }
+
+        [MaxLength(45), MinLength(5), Required]
         public string Name { get; set; }
+
+        [MaxLength(45), MinLength(5), Required]
         public string ApiEntryPoint { get; set; }
+
+        [MaxLength(45), MinLength(5), Required]
         public string Info { get; set; }
     }
 }
