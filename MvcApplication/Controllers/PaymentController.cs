040fbfe692b70659ebf4f58dcc278662d0086be5
Andrei Caprar <andrei.caprar@evozon.com>
2017-05-09 18:19:41 +0300

    Added payment context logic

--- MvcApplication/Controllers/PaymentController.cs
+++ MvcApplication/Controllers/PaymentController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MvcApplication.Bundles.Payments.Context;
+using MvcApplication.Bundles.Payments.Entity;
+using MvcApplication.Config.Users;
 
 namespace MvcApplication.Controllers
 {
     public class PaymentController : Controller
     {
+        private string _connectionString;
+
+        public PaymentController(ConnectionConfiguration connection)
+        {
+            _connectionString = connection.ConnectionString;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -11,6 +23,17 @@ namespace MvcApplication.Controllers
 
         public void DownloadPdfPayment(int paymentId)
         {
+            Payment payment = null;
+            using (var paymentCtx = new PaymentDbContext(_connectionString))
+            {
+                payment = paymentCtx.Payments.Where(p => p.Id == paymentId).ToList().First();
+            }
+
+            if (payment == null)
+            {
+                throw new Exception("No payment was found");
+            }
+
             // TODO: make logic to download payment
         }
     }
