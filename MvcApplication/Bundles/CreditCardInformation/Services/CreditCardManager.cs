bd071aede0822f0684c651f9dca792b37a82f9d1
Andrei Caprar <andrei.caprar@evozon.com>
2017-05-18 18:25:47 +0300

    Added Credit card manager class

---
+++ MvcApplication/Bundles/CreditCardInformation/Services/CreditCardManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MvcApplication.Bundles.CreditCardInformation.Context;
+using MvcApplication.Bundles.CreditCardInformation.Entity;
+
+namespace MvcApplication.Bundles.CreditCardInformation.Services
+{
+    public class CreditCardManager
+    {
+        private readonly string _connectionString;
+
+        public CreditCardManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+        public List<CreditCard> GetUserCreditCards(int id)
+        {
+            List<CreditCard> creditCards;
+            using (var ccCtx = new CreditCardDbContext(_connectionString))
+            {
+                creditCards = ccCtx.CreditCards.Where(cc => cc.UserId == id).ToList();
+            }
+
+            return creditCards;
+        }
+
+        public void ChangeCardPin(int creditCardId, string pinNumber)
+        {
+            try
+            {
+                using (var userCtx = new CreditCardDbContext(_connectionString))
+                {
+                    var creditCard = userCtx.CreditCards.Where(cc => cc.Id == creditCardId).ToList().First();
+
+                    creditCard.PinNumber = pinNumber;
+                    userCtx.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void Change3dSecureStatus(int creditCardId, bool status)
+        {
+            try
+            {
+                using (var userCtx = new CreditCardDbContext(_connectionString))
+                {
+                    var creditCard = userCtx.CreditCards.Where(cc => cc.Id == creditCardId).ToList().First();
+
+                    creditCard.Security3D = status;
+                    userCtx.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
--- MvcApplication/Controllers/CreditCardInformationController.cs
+++ MvcApplication/Controllers/CreditCardInformationController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using MvcApplication.Bundles.CreditCardInformation.Context;
-using MvcApplication.Bundles.CreditCardInformation.Entity;
+using MvcApplication.Bundles.CreditCardInformation.Services;
 using MvcApplication.Config.Connection;
 
 namespace MvcApplication.Controllers
@@ -13,58 +9,30 @@ namespace MvcApplication.Controllers
     {
         private readonly string _connectionString;
 
+        private readonly CreditCardManager _creditCardManager;
+
         public CreditCardInformationController(IOptions<ConnectionConfiguration> configuration)
         {
             _connectionString = configuration.Value.ConnectionString;
+
+            _creditCardManager = new CreditCardManager(configuration.Value.ConnectionString);
         }
 
         public ViewResult Index(int id)
         {
-            List<CreditCard> creditCards;
-            using (var ccCtx = new CreditCardDbContext(_connectionString))
-            {
-                creditCards = ccCtx.CreditCards.Where(cc => cc.UserId == id).ToList();
-            }
+            var creditCards = _creditCardManager.GetUserCreditCards(id);
 
             return View(creditCards);
         }
 
         public void ChangePinNumber(int creditCardId, string pinNumber)
         {
-            try
-            {
-                using (var userCtx = new CreditCardDbContext(_connectionString))
-                {
-                    var creditCard = userCtx.CreditCards.Where(cc => cc.Id == creditCardId).ToList().First();
-
-                    creditCard.PinNumber = pinNumber;
-                    userCtx.SaveChanges();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            _creditCardManager.ChangeCardPin(creditCardId, pinNumber);
         }
 
         public void Manage3DSecure(int creditCardId, bool status)
         {
-            try
-            {
-                using (var userCtx = new CreditCardDbContext(_connectionString))
-                {
-                    var creditCard = userCtx.CreditCards.Where(cc => cc.Id == creditCardId).ToList().First();
-
-                    creditCard.Security3D = status;
-                    userCtx.SaveChanges();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            _creditCardManager.Change3dSecureStatus(creditCardId, status);
         }
     }
 }
