66d596bb5a7ab6336566ac30a3a71c7748546c16
Andrei Caprar <andrei.caprar@evozon.com>
2017-05-18 18:28:41 +0300

    added http verbs for controller

--- MvcApplication/Controllers/CreditCardInformationController.cs
+++ MvcApplication/Controllers/CreditCardInformationController.cs
@@ -7,17 +7,14 @@ namespace MvcApplication.Controllers
 {
     public class CreditCardInformationController : Controller
     {
-        private readonly string _connectionString;
-
         private readonly CreditCardManager _creditCardManager;
 
         public CreditCardInformationController(IOptions<ConnectionConfiguration> configuration)
         {
-            _connectionString = configuration.Value.ConnectionString;
-
             _creditCardManager = new CreditCardManager(configuration.Value.ConnectionString);
         }
 
+        [HttpGet]
         public ViewResult Index(int id)
         {
             var creditCards = _creditCardManager.GetUserCreditCards(id);
@@ -25,11 +22,13 @@ namespace MvcApplication.Controllers
             return View(creditCards);
         }
 
+        [HttpPut]
         public void ChangePinNumber(int creditCardId, string pinNumber)
         {
             _creditCardManager.ChangeCardPin(creditCardId, pinNumber);
         }
 
+        [HttpPut]
         public void Manage3DSecure(int creditCardId, bool status)
         {
             _creditCardManager.Change3dSecureStatus(creditCardId, status);
